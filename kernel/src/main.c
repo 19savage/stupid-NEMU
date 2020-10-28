35cda7c007be85371739c702e9681df56635249a
tracer-ics2016 <tracer@njuics.org>
2020-10-28 14:23:01 +0000

    >  compile NEMU 3018001564 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:23:01 up 15 days,  5:33,  3 users,  load average: 0.46, 0.18, 0.11 5803f6b66e32c063816a42952002c5cbf0248e86

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -23,7 +23,9 @@ void init() {
 #ifdef IA32_PAGE
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
-	 * Before setting up correct paging, no global variable can be used. */
+	
+ * Before setting up correct paging, no global variable can be used. */
+	set_bp();
 	init_page();
 
 	/* After paging is enabled, transform %esp to virtual address. */
