fb153cee649fbbb0b8e4e8bb8a7acc66f37906c8
tracer-ics2016 <tracer@njuics.org>
2020-12-31 07:06:50 +0000

    >  compile NEMU 3018001564 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:06:50 up 25 min,  1 user,  load average: 1.27, 0.72, 0.39 661214ccc9c45b6340ac1c454793702792718904

--- testcase/src/hello-inline-asm.c
+++ testcase/src/hello-inline-asm.c
@@ -1,12 +1,13 @@
 #include "trap.h"
 
-const char str[] = "Hello , world!\n";
+const char str[] = "Hello, world!\n";
 
-int main(){
-	asm volatile ( "movl $4 , %eax;"
-			"movl $1 , %ebx;"
-			"movl $str , %ecx;"
-			"movl $14 , %edx;"
-			"int $0x80");
+int main() {
+	asm volatile (  "movl $4, %eax;"	// system call ID, 4 = SYS_write
+					"movl $1, %ebx;"	// file descriptor, 1 = stdout
+					"movl $str, %ecx;"	// buffer address
+					"movl $14, %edx;"	// length
+					"int $0x80");
+
 	return 0;
 }
